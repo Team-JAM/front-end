fdb8f4e244edc89e38514c6f4d49480b1676cbfd
Jonathan Ho <johnnyho82@gmail.com>
2020-03-02 10:33:11 -0500

    Comment out console.log

--- src/data/dummyGrid.js
+++ src/data/dummyGrid.js
@@ -9,11 +9,11 @@ for (let i = 0; i <= dimension; i++) {
 }
 
 for (const room of Object.values(dummyData)) {
-	console.log(room.room_id);
+	// console.log(room.room_id);
 	const coordinates = room.coordinates.slice(1, -1).split(',');
 	const xCoordinate = coordinates[0];
 	const yCoordinate = coordinates[1];
-	console.log(xCoordinate, yCoordinate);
+	// console.log(xCoordinate, yCoordinate);
 	dummyGrid[yCoordinate][xCoordinate] = room;
 }
 
