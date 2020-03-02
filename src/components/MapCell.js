715e97cb3bf7f0bf1a22a51d934b994328c75602
Jonathan Ho <johnnyho82@gmail.com>
2020-03-02 14:43:48 -0500

    Add prelimiinary map styles

--- src/components/MapCell.js
+++ src/components/MapCell.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { useDataContext } from '../contexts/DataContext';
+
 export default function MapCell({ cell }) {
-	return <StyledCell>{cell.room_id}</StyledCell>;
+	const {
+		data: { roomData },
+	} = useDataContext();
+
+	// console.log('MAPCELL CELL', cell);
+	// console.log('MAPCELL ROOMDATA', roomData);
+
+	const isCurrentRoom = roomData.room_id === cell.room_id ? true : false;
+
+	return (
+		<StyledCell
+			isCurrentRoom={isCurrentRoom}
+			terrain={cell.terrain}
+			elevation={cell.elevation}>
+			{cell.room_id}
+		</StyledCell>
+	);
 }
 
 const StyledCell = styled.div`
-	border: 1px solid silver;
 	width: 6rem;
 	height: 6rem;
+
+	border: 1px solid lightskyblue;
+	border: ${props => props.terrain === 'NORMAL' && '1px solid brown'};
+	border: ${props => props.isCurrentRoom && '2px solid red'};
+
+	background-color: lightcyan;
+	background-color: ${props => props.terrain === 'NORMAL' && '#feb950'};
+	background-color: ${props => props.terrain === 'CAVE' && '#281300'};
+	background-color: ${props => props.terrain === 'MOUNTAIN' && '#991e00'};
+	background-color: ${props => props.terrain === 'TRAP' && 'red'};
+	background-color: ${props => props.isCurrentRoom && 'white'};
 `;
