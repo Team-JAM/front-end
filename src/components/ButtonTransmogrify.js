6b4cdf189f4bfdf2316c704dd01cf2587838de5f
Jonathan Ho <johnnyho82@gmail.com>
2020-03-05 18:38:33 -0500

    Add ButtonExamineBook

--- src/components/ButtonTransmogrify.js
+++ src/components/ButtonTransmogrify.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 import { useDataContext } from '../contexts/DataContext';
+import { useGetStatus } from '../hooks/useGetStatus';
+import { useGetBalance } from '../hooks/useGetBalance';
+import { sleep } from '../utils/sleep';
 import { specialRooms } from '../data/specialRooms';
 
 export default function ButtonTransmogrify({ item }) {
@@ -8,6 +11,8 @@ export default function ButtonTransmogrify({ item }) {
 		data: { roomData },
 		dispatch,
 	} = useDataContext();
+	const getStatus = useGetStatus();
+	const getBalance = useGetBalance();
 
 	const handleClick = name => {
 		dispatch({ type: 'GET_DATA_START' });
@@ -15,70 +20,21 @@ export default function ButtonTransmogrify({ item }) {
 		(async () => {
 			try {
 				const res = await axiosWithAuth().post('/adv/transmogrify/', { name });
+				console.log(res.data);
 
 				dispatch({ type: 'GET_DATA_SUCCESS', payload: res.data });
 
-				const cooldown = res.data.cooldown;
-
-				const asyncList = [getPlayerStatus, getBalance];
-
-				const sleep = ms =>
-					new Promise(resolve => {
-						setTimeout(resolve, ms);
-					});
-
-				await sleep(cooldown * 1000);
+				const asyncList = [() => getStatus(res.data.cooldown), getBalance];
 
 				for (const asyncFunction of asyncList) {
 					const cooldown = await asyncFunction();
-					await sleep(cooldown * 1000);
+					await sleep(cooldown);
 				}
 			} catch (err) {
 				console.log(err);
 				dispatch({ type: 'GET_DATA_FAILURE' });
 			}
 		})();
-		// axiosWithAuth()
-		// 	.post('/adv/transmogrify/', { name })
-		// 	.then(res => {
-		// 		console.log(res.data);
-		// 		dispatch({ type: 'GET_DATA_SUCCESS', payload: res.data });
-		// 	})
-		// 	.catch(err => {
-		// 		console.log(err);
-		// 		dispatch({ type: 'GET_DATA_FAILURE' });
-		// 	});
-	};
-
-	const getPlayerStatus = async () => {
-		dispatch({ type: 'GET_DATA_START' });
-
-		try {
-			const res = await axiosWithAuth().post('/adv/status/');
-			// localStorage.setItem('name', res.data.name)
-
-			dispatch({ type: 'GET_STATUS_SUCCESS', payload: res.data });
-
-			return res.data.cooldown;
-		} catch (err) {
-			console.log(err);
-			dispatch({ type: 'GET_DATA_FAILURE' });
-		}
-	};
-
-	const getBalance = async () => {
-		dispatch({ type: 'GET_DATA_START' });
-
-		try {
-			const res = await axiosWithAuth().get('/bc/get_balance');
-
-			dispatch({ type: 'GET_BALANCE_SUCCESS', payload: res.data.messages[0] });
-
-			return res.data.cooldown;
-		} catch (err) {
-			console.log(err);
-			dispatch({ type: 'GET_DATA_FAILURE' });
-		}
 	};
 
 	return (
