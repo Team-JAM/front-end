891f33dc4d2be34739fe7667de883db8f492f430
allisonkydy <allisnelly@gmail.com>
2020-03-12 13:44:34 -0500

    remove JAMcode logic & state from reducer

--- src/reducers/dataReducer.js
+++ src/reducers/dataReducer.js
@@ -27,7 +27,6 @@ export const initialDataState = {
 	isLoading: false,
 	error: '',
 	treasureMode: false,
-	isJAM: localStorage.getItem('isJAM'),
 };
 
 export const dataReducer = (state = initialDataState, action) => {
@@ -188,11 +187,6 @@ export const dataReducer = (state = initialDataState, action) => {
 			return {
 				...state,
 				treasureMode: !state.treasureMode,
-			};
-		case 'SET_IS_JAM':
-			return {
-				...state,
-				isJAM: true,
 			};
 		default:
 			return state;
