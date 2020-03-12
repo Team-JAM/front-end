8042ce852bbf9c01bbb783375118fcfc6a78903a
allisonkydy <allisnelly@gmail.com>
2020-03-12 13:43:29 -0500

    remove pusher, undo login page rendering from App

--- src/App.js
+++ src/App.js
@@ -3,31 +3,20 @@ import { Route } from 'react-router-dom';
 import './App.css';
 import GlobalStyle from './styled-components/GlobalStyle';
 
-// import DataProvider from './contexts/DataContext';
+import DataProvider from './contexts/DataContext';
 
-import { useDataContext } from './contexts/DataContext';
-
-import { NavBar, Game, About, Login } from './components';
+import { NavBar, Game, About } from './components';
 
 function App() {
-	const {
-		data: { isJAM },
-	} = useDataContext();
 
 	return (
 		<div className='App'>
 			<GlobalStyle />
-			{/* <DataProvider> */}
-			{isJAM ? (
-				<>
+			<DataProvider>
 					<NavBar />
 					<Route exact path='/' component={Game} />
 					<Route path='/about' component={About} />
-				</>
-			) : (
-				<Login />
-			)}
-			{/* </DataProvider> */}
+			</DataProvider>
 		</div>
 	);
 }
